eeff5d64d6405f2a593621540052d5806a1e54ed
Karan Shende <161307512+karnn1010@users.noreply.github.com>
2026-05-20 17:24:32 +0530

    Enhance smoothie app with ingredient search functionality
    Updated the smoothie app to include search values for ingredients and improved data handling.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import pandas as pd
 
 # Write directly to the app
 st.title(" :cup_with_straw: Costomize Your Smoothie !  :cup_with_straw:  ")
@@ -19,22 +20,37 @@ st.write('The name on your Smoothie will be ', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table(
+"SMOOTHIES.PUBLIC.FRUIT_OPTIONS"
+).select(
+col('FRUIT_NAME'),
+col('SEARCH_ON')
+)
 # st.dataframe(data=my_dataframe, use_container_width=True)
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 inngredients :'
     , my_dataframe
-    , max_selections = 5 
+    , max_selections = 5
 )
 
 import requests
+
 if ingredients_list:
   
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+      
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
